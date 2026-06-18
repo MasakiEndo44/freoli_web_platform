2f68631e5c1ad441ae7b4b1290c85bf9770cd06d
aberyooooo <aberyo0801@gmail.com>
2026-06-19 00:54:22 +0900

    Update hero image and Padoma listen link

---
+++ data/releases.ts
@@ -0,0 +1,13 @@
+export type FeaturedRelease = {
+  title: string;
+  artist: string;
+  url: string;
+  description: string;
+};
+
+export const featuredRelease: FeaturedRelease = {
+  title: "パドマ",
+  artist: "FREOLI",
+  url: "https://linkco.re/vU38MtGM",
+  description: "1st single",
+};
